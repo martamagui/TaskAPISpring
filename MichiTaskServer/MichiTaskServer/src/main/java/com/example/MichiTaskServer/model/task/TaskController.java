636790087ba531c5a3653d42a341d0781fba65d5
Marta Molina <marta.molina@live.u-tad.com>
2022-01-23 20:43:47 +0100

    Task controller and service

--- MichiTaskServer/MichiTaskServer/src/main/java/com/example/MichiTaskServer/model/task/TaskController.java
+++ MichiTaskServer/MichiTaskServer/src/main/java/com/example/MichiTaskServer/model/task/TaskController.java
@@ -1,4 +1,42 @@
 package com.example.MichiTaskServer.model.task;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/tasks")
 public class TaskController {
+    private TaskService taskService;
+
+    @Autowired
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    public List<Task> getTasks() {
+        return taskService.getTasks();
+    }
+
+    @PostMapping
+    public void addTask(Task task) {
+        taskService.addTask(task);
+    }
+
+    @DeleteMapping(path = "{taskId}")
+    public void deleteUser(@PathVariable("taskId") Integer taskId) {
+        taskService.delete(taskId);
+    }
+
+    //No pongo que se pueda cambiar el fk de la lista porque de moento no panteo que se puedan migrar als tareas de lista a lista
+    @PutMapping(path = "{taskId}")
+    public void editTask(@PathVariable("taskId") Integer taskId,
+                         @RequestParam(required = false) String taskTitle,
+                         @RequestParam(required = false) String description,
+                         @RequestParam(required = false) String state) {
+        taskService.editTask(taskId,taskTitle,description,state);
+
+    }
 }
--- MichiTaskServer/MichiTaskServer/src/main/java/com/example/MichiTaskServer/model/task/TaskService.java
+++ MichiTaskServer/MichiTaskServer/src/main/java/com/example/MichiTaskServer/model/task/TaskService.java
@@ -1,4 +1,48 @@
 package com.example.MichiTaskServer.model.task;
 
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+@Service
 public class TaskService {
+    private TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+    public List<Task> getTasks(){return taskRepository.findAll();}
+
+    public void addTask(Task task) {
+        if(taskRepository.findTaskByTaskId(task.getTaskId()).isPresent()){
+            throw new IllegalStateException("Task Id already taken.");
+        }else{
+            taskRepository.save(task);
+        }
+    }
+
+    public void delete(Integer taskId) {
+        if(taskRepository.findTaskByTaskId(taskId).isPresent()){
+            taskRepository.deleteById(taskId);
+        }else{
+            throw new IllegalStateException("This Task doesn't exist.");
+        }
+    }
+    @Transactional
+    public void editTask(Integer taskId, String taskTitle, String description, String state) {
+        if(taskRepository.findTaskByTaskId(taskId).isPresent()){
+            Task task = taskRepository.findTaskByTaskId(taskId).get();
+            if(taskTitle!=null){
+                task.setTitle(taskTitle);
+            }
+            if(description!=null){
+                task.setDescription(description);
+            }
+            if(state!=null){
+                task.setState(state);
+            }
+        }else{
+            throw new IllegalStateException("This Task doesn't exist.");
+        }
+    }
 }
